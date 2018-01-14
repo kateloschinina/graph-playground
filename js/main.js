aff576e552b981a70bbd286242233d665461b4ab
Katerina <katerina.loschinina@ft.com>
2018-01-14 22:49:29 +0000

    draw a dot

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+const verticesColor = "#77d";
+
 function draw () {
-    setUpCanvas();
+    let cx = setUpCanvas();
+    drawDot(cx, 100, 100);
     return true;
+}
+
+function drawDot (cx, x, y, r = 3) {
+    cx.beginPath();
+    cx.arc(x, y, r, 0, Math.PI * 2);
+    cx.fillStyle = verticesColor;
+    cx.fill();
 }
 
 function setUpCanvas () {
@@ -9,4 +19,5 @@ function setUpCanvas () {
     const windowSize = { width: window.innerWidth, height: window.innerHeight };
     cx.canvas.width = windowSize.width;
     cx.canvas.height = windowSize.height;
+    return cx;
 }
