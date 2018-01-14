1beb5056eb5d2d2227af745a9c25ff9b41bc748a
Katerina <katerina.loschinina@ft.com>
2018-01-14 20:38:25 +0000

    set up canvas function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 function draw () {
+    setUpCanvas();
+    return true;
+}
+
+function setUpCanvas () {
     let canvas = document.querySelector("#path-finder");
     let cx = canvas.getContext("2d");
     const windowSize = { width: window.innerWidth, height: window.innerHeight };
     cx.canvas.width = windowSize.width;
     cx.canvas.height = windowSize.height;
-    return true;
 }
